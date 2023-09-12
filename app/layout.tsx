1c0bd2c233e01c0c91cf465fe55300c9cde15806
Karasev Vladimir <79168772711@ya.ru>
2023-09-12 15:15:12 +0300

    fix: add webmanifest, fix favicons for different devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,37 +5,7 @@ import { osans, inter, lato } from "./fonts";
 export const metadata: Metadata = {
   title: "Dagin Pets",
   description: "Перевозка питомцев в ОАЭ",
-  icons: [
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "32x32",
-      url: "/favicons/favicon-32x32.png",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "16x16",
-      url: "/favicons/favicon-16x16.png",
-    },
-    {
-      rel: "apple-touch-icon",
-      sizes: "180x180",
-      url: "/favicons/apple-touch-icon.png",
-    },
-    {
-      rel: "icon",
-      url: "/favicons/android-chrome-192x192.png",
-      type: "image/png",
-      sizes: "192x192",
-    },
-    {
-      rel: "icon",
-      url: "/favicons/android-chrome-512x512.png",
-      type: "image/png",
-      sizes: "512x512",
-    },
-  ],
+  manifest: "/manifest.webmanifest",
 };
 
 export default function RootLayout({
