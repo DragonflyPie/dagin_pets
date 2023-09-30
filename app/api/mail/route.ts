08ec63d8c64526fd5032e20f2a4a05ed00a2d423
Karasev Vladimir <79168772711@ya.ru>
2023-09-30 15:00:22 +0300

    debug: fix typo

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -27,7 +27,7 @@ export async function POST(request: Request) {
     let transporter = nodemailer.createTransport({
       name: "timeweb.SMTP",
       port: 465,
-      host: " ssl://smtp.timeweb.ru",
+      host: "ssl://smtp.timeweb.ru",
       auth: {
         user: email,
         pass: password,
