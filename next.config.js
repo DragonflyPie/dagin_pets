9a0d947e343d0ca1d79114493ac14cc483e9083a
Karasev Vladimir <79168772711@ya.ru>
2023-10-14 15:14:48 +0300

    build: update dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /* config options here */
+};
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-module.exports = withBundleAnalyzer({
-  // your Next.js configuration
-});
+module.exports = nextConfig;
