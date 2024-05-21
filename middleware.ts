cbb7f25b6a6ddac697f3173716ece338cdb042ee
Karasev Vladimir <79168772711@ya.ru>
2024-05-21 17:05:55 +0300

    fix: try to fix matcher in middleware for robots.txt

--- middleware.ts
+++ middleware.ts
@@ -38,9 +38,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
-  matcher: [
-    // "/((?!api|_next/static|_next/image|images|favicon.ico|favicons|manifest.webmanifest).*)",
-    "/",
-  ],
+  matcher: ["/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)"],
 };
