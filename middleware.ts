bffcc37bbb508e81debac6343ff60f7131f91fa5
Karasev Vladimir <79168772711@ya.ru>
2024-05-21 18:50:49 +0300

    chore: fix typo in ru header description

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,6 @@ import type { NextRequest } from "next/server";
 
 import { i18n } from "@/i18n.config";
 
-import { match as matchLocale } from "@formatjs/intl-localematcher";
-import Negotiator from "negotiator";
-
 // function getLocale(request: NextRequest): string | undefined {
 //   const negotiatorHeaders: Record<string, string> = {};
 //   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
@@ -39,9 +36,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
-  matcher: [
-    // "/((?!api|_next/static|_next/image|images|favicon.ico|favicons|manifest.webmanifest).*)",
-    "/",
-  ],
+  matcher: ["/"],
 };
