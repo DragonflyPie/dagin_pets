fd241b6658468474d725b6a26aca6f06eb60474a
Karasev Vladimir <79168772711@ya.ru>
2024-05-27 16:31:33 +0300

    debug: trying to fix fb pixel again

--- app/_utilities/metrics/FacebookPixel.js
+++ app/_utilities/metrics/FacebookPixel.js
@@ -27,20 +27,19 @@ const FacebookPixel = () => {
         id="fb-pixel"
         strategy="afterInteractive"
         onLoad={() => setLoaded(true)}
-        data-pixel-id={FB_PIXEL_ID}
       >
         {`
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '419616927315386');
-fbq('track', 'PageView');
-`}
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '${FB_PIXEL_ID}');
+      fbq('track', 'PageView');
+    `}
       </Script>
       <noscript>
         <Image
